8424599a061a506cc71ebf74aa0904a0417ab966
Bart Thate <bthate@dds.nl>
2024-04-02 20:00:05 +0200

    add missing

---
+++ botl/repeater.py
@@ -0,0 +1,31 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,W0105,W0718
+
+
+"repeater"
+
+
+from .timer  import Timer
+from .thread import launch
+
+
+class Repeater(Timer):
+
+    "Repeater"
+
+    def run(self):
+        launch(self.start)
+        super().run()
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'Repeater',
+    )
+
+
+__all__ = __dir__()
---
+++ botl/utils.py
@@ -0,0 +1,58 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,W0105,E0402
+
+
+"utils"
+
+
+def laps(seconds, short=True):
+    "show elapsed time."
+    txt = ""
+    nsec = float(seconds)
+    if nsec < 1:
+        return f"{nsec:.2f}s"
+    yea = 365*24*60*60
+    week = 7*24*60*60
+    nday = 24*60*60
+    hour = 60*60
+    minute = 60
+    yeas = int(nsec/yea)
+    nsec -= yeas*yea
+    weeks = int(nsec/week)
+    nsec -= weeks*week
+    nrdays = int(nsec/nday)
+    nsec -= nrdays*nday
+    hours = int(nsec/hour)
+    nsec -= hours*hour
+    minutes = int(nsec/minute)
+    nsec -= int(minute*minutes)
+    sec = int(nsec)
+    if yeas:
+        txt += f"{yeas}y"
+    if weeks:
+        nrdays += weeks * 7
+    if nrdays:
+        txt += f"{nrdays}d"
+    if short and txt:
+        return txt.strip()
+    if hours:
+        txt += f"{hours}h"
+    if minutes:
+        txt += f"{minutes}m"
+    if sec:
+        txt += f"{sec}s"
+    txt = txt.strip()
+    return txt
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'laps',
+    )
+
+
+__all__ = __dir__()
