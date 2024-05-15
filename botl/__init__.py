b875533ab311999c1d1620860809b49a618a3981
Bart Thate <bthate@dds.nl>
2024-05-15 05:02:03 +0200

    add botl

---
+++ botl/__init__.py
@@ -0,0 +1,4 @@
+# This file is placed in the Public Domain.
+
+
+"Reconsider OTP-CR-117/19"
---
+++ botl/utils.py
@@ -0,0 +1,24 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"utilities"
+
+
+import datetime
+
+
+def hms():
+    "return hour:minutes:seconds string."
+    return now().split(".")[0]
+
+
+def date():
+    "return time with date."
+    return str(datetime.datetime.now())
+
+
+def now():
+    "return string of the current time."
+    return date().split()[-1]
